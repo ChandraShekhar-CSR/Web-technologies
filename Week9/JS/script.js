459a5c545ce74309df6e1074d152a7b4cecd1f86
ChandraShekhar-CSR <NP03CS4A220272@heraldcollege.edu.np>
2023-02-11 18:50:54 +0545

    initial commit

---
+++ Week9/JS/script.js
@@ -0,0 +1,29 @@
+alert("Hey girl \nwanna be mine");
+console.log("My name is CSR");
+console.log("wake up to reality");
+console.log(document.getElementById("paragraph").innerHTML);
+document.getElementById("paragraph").innerHTML="Yo, Konichiwa Sayonara";
+// document.getElementById("paragraph").style.color="brown";
+// document.getElementById("paragraph").style.background="aqua";
+document.getElementById("paragraph").style.cssText="color:brown; background:aqua;";
+
+var a=1;
+var b="Shekhar";
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof document);
+
+var age=10;
+age=20;
+var age1=20;
+age=30;
+const age3=30;
+age=40;
+
+console.log(age);
+console.log(age1);
+console.log(age3);
+
+console.log(prompt("enter your name:"));
+document.getElementById("message").innerHTML="My name is "+b;
